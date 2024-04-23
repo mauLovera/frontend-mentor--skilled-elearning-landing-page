fd2d68ca7fd8807a367acd95ddca49c17f959f46
mauLovera <mauricioloveraedu@gmail.com>
2024-04-23 13:23:22 -0400

    feat: add button styles

--- src/components/ButtonOrLink.tsx
+++ src/components/ButtonOrLink.tsx
@@ -6,8 +6,13 @@ const buttonOrLinkStyles = cva(
   {
     variants: {
       intent: {
-        primary: "",
-        secondary: "",
+        header:
+          "text-button-sm-tall px-6 pt-[9px] pb-[11px] md:px-8 md:py-[14px] bg-blue hover:bg-blue-hover text-white rounded-[28px]",
+        primary:
+          "py-[15px] px-[38px] text-button-sm md:text-button-base md:py-4 md:px-8 lg:pt-[19px] lg:pb-4 bg-gradient-to-b from-orange to-hot-pink rounded-[31px] text-white",
+        secondary: "text-pink hover:text-pink-hover text-button-base",
+        footer:
+          "text-button-sm-tall px-[24px] pt-[9px] pb-[11px] md:px-8 md:py-[14px] bg-gradient-to-b from-purple to-hot-pink rounded-[28px] text-white",
       },
     },
     defaultVariants: {
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
+import { ButtonOrLink } from "./ButtonOrLink"
 import LogoIcon from "./icons/LogoIcon"
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between pt-4 md:pt-6">
       <LogoIcon dark />
+      <ButtonOrLink intent={"header"}>Get Started</ButtonOrLink>
     </header>
   )
 }
